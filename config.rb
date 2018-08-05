403b396da1296e23830249ee84984ba27905bb33
Pim Zonneveld <pimzonneveld@gmail.com>
2018-08-05 09:50:38 +0200

    Added dynamic pages, js img scroller, scroll change navbar

--- config.rb
+++ config.rb
@@ -20,3 +20,9 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+data.projects.each do |name, project|
+  p name
+  # p project
+  proxy "/projects/#{name}.html", "/projects/show.html", locals: { description: project.description, name: project.name, images: project.images }, ignore: true
+end
--- source/javascripts/navbarScroll.js
+++ source/javascripts/navbarScroll.js
@@ -4,13 +4,15 @@ if(window.location.pathname == "/"){
   var banner = document.querySelector(".banner")
   var navbar = document.querySelector(".navbar-wagon")
 
-  var sticky = banner.offsetHeight;
+  var sticky = banner.offsetHeight - 50;
 
   function myFunction() {
     if (document.body.scrollTop > sticky || document.documentElement.scrollTop > sticky) {
+      document.querySelector(".navbar-wagon-brand").innerHTML = "<img src='/images/bvbd-logo-dark.png' alt='logo'>"
       navbar.classList.remove("navbar-invisible")
       navbar.classList.add("navbar-visible")
     } else {
+      document.querySelector(".navbar-wagon-brand").innerHTML = "<img src='/images/bvbd-logo-light.png' alt='logo'>"
       navbar.classList.remove("navbar-visible")
       navbar.classList.add("navbar-invisible")
     }
