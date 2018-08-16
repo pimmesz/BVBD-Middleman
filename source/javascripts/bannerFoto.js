a265232c4020ad1548745b8f4c8a93570bbb34f9
Pim Zonneveld <pimzonneveld@gmail.com>
2018-08-16 09:31:18 +0200

    Self build slider

---
+++ source/javascripts/bannerFoto.js
@@ -0,0 +1,27 @@
+setInterval(change, 5000);
+
+function change(){
+  let homeActive = document.querySelector("#active");
+  let bannerActiveNumber = homeActive.dataset.number;
+  let bannerActive = findBackground(bannerActiveNumber);
+  if(bannerActiveNumber === "3") {
+    bannerNextNumber = 4;
+    bannerPrepareNumber = 1;
+  } else if(bannerActiveNumber === "4"){
+    bannerNextNumber = 1;
+    bannerPrepareNumber = 2;
+  } else{
+    bannerNextNumber = parseInt(bannerActiveNumber) + 1;
+    bannerPrepareNumber = parseInt(bannerActiveNumber) + 2;
+  }
+  let bannerNext = findBackground(`${parseInt(bannerNextNumber)}`);
+  let bannerPrepare = findBackground(`${parseInt(bannerPrepareNumber)}`);
+  bannerActive.id = "inactive";
+  bannerNext.id = "active";
+  bannerPrepare.id = "next";
+
+}
+
+function findBackground(number){
+  return document.querySelector(`[data-number='${number}']`);
+}
